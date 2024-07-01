b5b9c2f35a376e1687f8f9abd214bb9cf0bdba3d
Lucas Martins <lucasfranca_zs@hotmail.com>
2024-07-01 12:17:40 -0300

    Projeto Criado e Configurado conexão com MariaDB

---
+++ src/test/java/br/com/lmf/ControleContatos/ControleContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lmf.ControleContatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
