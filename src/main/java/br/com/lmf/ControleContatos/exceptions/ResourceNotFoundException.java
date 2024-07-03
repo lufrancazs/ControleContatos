a1eaa56b569c7beeef638f72c422929b1a7e70e9
Lucas Martins <lucasfranca_zs@hotmail.com>
2024-07-03 19:31:49 -0300

    Ajustado mensagem da exceção

--- src/main/java/br/com/lmf/ControleContatos/exceptions/ResourceNotFoundException.java
+++ src/main/java/br/com/lmf/ControleContatos/exceptions/ResourceNotFoundException.java
@@ -1,30 +1,15 @@
 package br.com.lmf.ControleContatos.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-
-public class ResourceNotFoundException extends AppException{
+public class ResourceNotFoundException extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 	
-	private Long resourceId;
-	
-	public ResourceNotFoundException(String message) {
-
-	}
-
-	public ResourceNotFoundException(Long resourceId) {
-		this.resourceId = resourceId;
+	public ResourceNotFoundException() {
+		
 	}
 	
-	@Override
-	public ProblemDetail toProblemDetail() {
-		ProblemDetail pb = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
-		
-		pb.setTitle("Recurso Não Encontrado");
-		pb.setDetail("Não existe o recurso com o id: " + resourceId + ".");
-		
-		return pb;
+	public ResourceNotFoundException(Object id) {
+		super("Recurso não encontrado. Id: " + id);
 	}
 
 }
